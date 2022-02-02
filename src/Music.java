c2a0c9b9651d7010aab2e014a4f5fdb1613007ee
Sitong_Lu <493984502@qq.com>
2022-02-01 23:07:05 -0700

    fix syntax error

--- src/Music.java
+++ src/Music.java
@@ -9,7 +9,7 @@ class Music extends Item{
     Music(String item_name, double purchase_price, int day_arrived, int new_condition, String new_band, String new_album){
         //set all instance variables/attributes inherited from Item class
         super(item_name,purchase_price,day_arrived,new_condition);
-        //set exculsive variables/attributes defined in Music class
+        //set exclusive variables/attributes defined in Music class
         setBand(new_band);
         setAlbum(new_album);
     }
