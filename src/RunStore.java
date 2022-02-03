c52876a7e87792bce90794e11f42ffcb3f5ead73
伦也 安芸 <493984502@qq.com>
2022-02-03 12:03:54 -0700

    run RunStore object in Main.java

--- src/RunStore.java
+++ src/RunStore.java
@@ -1,6 +1,13 @@
 public class RunStore {
 
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//        Store a = new Store();
+//        a.initialize(3);
+//        a.run(30);
+//        a.displayInventory();
+//    }
+
+    public static void run() {
         Store a = new Store();
         a.initialize(3);
         a.run(30);
