cc0087d1f00d665f33676652246463957f6ff5e4
Addzy <addzy94@gmail.com>
2022-02-04 13:40:20 -0700

    Sync

--- src/RunStore.java
+++ src/RunStore.java
@@ -2,17 +2,10 @@ import java.util.ArrayList;
 
 public class RunStore {
 
-//    public static void main(String[] args) {
-//        Store a = new Store();
-//        a.initialize(3);
-//        a.run(30);
-//        a.displayInventory();
-//    }
-
-    //to run it in Main.java
-    public static void run() {
+    public static void main(String[] args) {
         Store a = new Store();
         a.initialize(3);
         a.run(30);
+        a.displayInventory();
     }
 }
