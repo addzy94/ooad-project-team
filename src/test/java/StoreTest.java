2517864b5b3d0b6593bcdb2b2fd915b9e8a63abc
Sitong_Lu <493984502@qq.com>
2022-02-19 11:48:28 -0700

    add clerk test

---
+++ src/test/java/StoreTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class StoreTest {
+
+    @Test
+    public void initialize() {
+        Store test_store = new Store(30);
+
+        ArrayList<Staff> test_staff = test_store.getStaff();
+
+        assertEquals(test_staff.size(), 3); // Test if there are really 3 members in the Staff list at this moment
+        /*
+        Test if the stuff members in the stuff list are all Clerks
+         */
+        assertTrue(test_staff.get(0) instanceof Clerk);
+        assertTrue(test_staff.get(1) instanceof Clerk);
+        assertTrue(test_staff.get(2) instanceof Clerk);
+//        ArrayList<Staff> staff = new ArrayList<>();
+//        Clerk shaggy = new Clerk("Shaggy", 0, 79, new HaphazardTuningStrategy());
+//        Clerk velma = new Clerk("Velma", 0, 94, new ManualTuningStrategy());
+//        Clerk daphne = new Clerk("Daphne", 0, 36, new ElectronicTuningStrategy());
+//        staff.add(shaggy);
+//        staff.add(velma);
+//        staff.add(daphne);
+//        assertEquals(test_store.getStaff().size(), staff.size());
+    }
+}
