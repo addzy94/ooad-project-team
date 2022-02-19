5c96aae1fefd083f77e2ce8dd1ef5c932979a705
Sitong_Lu <493984502@qq.com>
2022-02-19 14:46:27 -0700

    test

--- src/test/java/StoreTest.java
+++ src/test/java/StoreTest.java
@@ -8,12 +8,14 @@ JUnit version: JUnit 4
  */
 public class StoreTest {
 
+    /*
+    Initialize a store object for testing
+     */
+    Store test_store = new Store(1);
+    ArrayList<Staff> test_staff = test_store.getStaff();
+
     @Test
     public void initialize() {
-        Store test_store = new Store(30);
-
-        ArrayList<Staff> test_staff = test_store.getStaff();
-
         assertEquals(test_staff.size(), 3); // Check if there are really 3 members in the Staff list at this moment
         /*
         Check if the stuff members in the stuff list are all Clerks
@@ -21,13 +23,13 @@ public class StoreTest {
         assertTrue(test_staff.get(0) instanceof Clerk);
         assertTrue(test_staff.get(1) instanceof Clerk);
         assertTrue(test_staff.get(2) instanceof Clerk);
-//        ArrayList<Staff> staff = new ArrayList<>();
-//        Clerk shaggy = new Clerk("Shaggy", 0, 79, new HaphazardTuningStrategy());
-//        Clerk velma = new Clerk("Velma", 0, 94, new ManualTuningStrategy());
-//        Clerk daphne = new Clerk("Daphne", 0, 36, new ElectronicTuningStrategy());
-//        staff.add(shaggy);
-//        staff.add(velma);
-//        staff.add(daphne);
-//        assertEquals(test_store.getStaff().size(), staff.size());
+    }
+
+    @Test
+    public void resetWorkDays() {
+        //Clerk c = test_store.chooseClerk();
+        test_store.run(1);
+        assertEquals(test_store.getDay(), 3);
+        assertFalse(true);
     }
 }
