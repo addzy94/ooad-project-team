da10c54fc71566797ef6c4979d4c724d7f20e8b8
伦也 安芸 <493984502@qq.com>
2022-03-01 12:15:38 -0700

    edit RunStore to generate number of days between 10 and 30 randomly

--- src/main/java/RunStore.java
+++ src/main/java/RunStore.java
@@ -3,6 +3,7 @@ public class RunStore {
     // Main method that simulates the Store
     public static void main(String[] args) {
         Store a = new Store(3); // Creates a store with 3 items of each type of the lowest level
-        a.run(30); // Runs the store for 30 days
+        int days = Helper.random.nextInt(30 - 10 + 1) + 10; // Generate number of days to run between 10 and 30
+        a.run(days); // Runs the store with that given number of days automatically, plus an extra day that allows the user to play as a customer
     }
 }
